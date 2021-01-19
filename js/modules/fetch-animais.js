9190359d7df4b0b18b2862702a6de0ce81b01758
Flávio Henrique <flaviokluppell3.0@outlook.com>
2021-01-19 09:49:14 -0300

    first commit

---
+++ js/modules/fetch-animais.js
@@ -0,0 +1,28 @@
+import initAnimaNumero from './anima-numeros.js';
+
+export default function initFetchAnimais() {
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+
+    return div;
+  }
+
+  async function fetchAnimais(url) {
+    try {
+      const animaisJSON = await (await fetch(url)).json();
+      const sectionNumeros = document.querySelector('.numeros-grid');
+
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        sectionNumeros.appendChild(divAnimal);
+      });
+      initAnimaNumero();
+    } catch (erro) {
+      console.log(Error(erro));
+    }
+  }
+
+  fetchAnimais('./animaisapi.json');
+}
